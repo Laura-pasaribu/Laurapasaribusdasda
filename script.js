43bead3bd655dc1988f243b24ec9356ddedc7f20
laurapasibu <laurapasaribu886@gmail.com>
2023-12-09 16:45:40 +0700

    initial commit

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("menu-icon").addEventListener("click", function() {
-  var menu = document.getElementById("menu");
-  if (menu.style.display === "block") {
-      menu.style.display = "none";
-  } else {
-      menu.style.display = "block";
-  }
-});
